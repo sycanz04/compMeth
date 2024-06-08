7114fd9ee172b47c6b6384569e74116bc2449cd5
sycanz04 <aidenchan0397@gmail.com>
2024-06-08 18:25:02 +0800

    feat: finished displaying necessary tables

---
+++ display.m
@@ -0,0 +1,4 @@
+function output= display()
+    serviceTime()
+    interArrival()
+    serviceType()
---
+++ serviceType.m
@@ -0,0 +1,9 @@
+function output= serviceType(x)
+    printf('\nService Type probability table\n');
+    printf('-----------------------------------------------------------\n');
+    printf('|Service Type    |Probability  |CDF          |Range       |\n');
+    printf('-----------------------------------------------------------\n');
+    printf('| 1              |0.25         |0.25         |1 - 25      |\n');
+    printf('| 2              |0.45         |0.70         |26 - 70     |\n');
+    printf('| 3              |0.30         |1.00         |71 - 100    |\n');
+    printf('-----------------------------------------------------------\n');
